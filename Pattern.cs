fab68b8005d944908a10aad6005e2eaac05231db
shivakar <shivakar@ch.iitr.ac.in>
2021-11-11 23:02:53 +0530

    [shivakar] ADD :regex for email validation

---
+++ Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace RegexPatternMaching
+{
+    class Pattern
+    {
+        public static string REJEX_Email = "^[a-zA-Z2]{3,8}([_|+|.|-]{0,1}[a-zA-Z0-9]{0,5}){0,1}[@][a-zA-Z0-9]{1,10}([.][a-z]{2,4}){1,2}$";
+      // UC1 EJEX_PINCODE = "^[1-9]{1}[0-9]{2}[\\s]{0,1}[0-9]{3}$"
+        public bool validatePinCode(string input)
+        {
+            return Regex.IsMatch(input, REJEX_Email);
+        }
+    }
+}
+ 
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RegexPatternMaching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+
+            bool result=(pattern.validatePinCode("abc-100@abc.net"));
+            if (result)
+                Console.WriteLine(" Valid Email!");
+            else
+                Console.WriteLine("Invalid Email!");
+            //valid:-
+            //abc@yahoo.com
+            // "abc.100@yahoo.com"
+            //"abc-100@yahoo.com"
+            //"abc111@abc.com"
+            //"abc-100@abc.net"
+            //"abc.100@abc.com.au"
+            //"abc@1.com"
+            // "abc@gmail.com.com"
+            // "abc+100@gmail.com"
+
+
+            //invalid:-
+            // "abc()*@gmail.com"
+            //"abc@%*.com"
+            //"abc..2002@gmail.com"
+            //"abc@abc@gmail.com"
+            //"abc@gmail.com.1a"
+            //"abc@gmail.com.aa.au"
+        }
+    }
+}
